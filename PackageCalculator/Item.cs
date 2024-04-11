5003bbc2b5785961e4998b4806284c804a1bbb64
MostafaAmiri1 <amiri.mostafa45@gmail.com>
2024-04-11 18:02:54 +0200

    first commit

--- PackageCalculator/Item.cs
+++ PackageCalculator/Item.cs
@@ -2,7 +2,9 @@ namespace PackageCalculator;
 
 public class Item
 {
-    public string Description { get; set; }
-    public int[] Dimensions { get; set; }
-    public int Weight { get; set; }
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public double Length { get; set; }
+    public double Width { get; set; }
+    public double Height { get; set; }
 }
